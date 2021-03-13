028849f7f567ccc26a2e47ff8c61f7294b388258
Roland Schläfli <rolandschlaefli@gmail.com>
2021-03-13 22:10:24 +0100

    fix: ensure that react-in-jsx-scope is also disabled for TS projects

--- packages/react-ts/.eslintrc.js
+++ packages/react-ts/.eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   extends: ['airbnb-typescript', 'prettier'],
   plugins: [],
-  rules: {},
+  rules: {
+    'react/react-in-jsx-scope': 0,
+  },
   parserOptions: {
     // tsconfigRootDir: __dirname,
     project: './tsconfig.json',
